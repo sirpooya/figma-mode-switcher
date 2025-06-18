f130c483d4cad33bab2cb71c787dfab3d0d0b103
Pooya <sirpooya@users.noreply.github.com>
2025-06-18 16:11:01 +0330

    updated harcoded colors w/ figma  base.css variables

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // Show the plugin UI with increased height to accommodate dropdown
-figma.showUI(__html__, { width: 300, height: 300 });
+figma.showUI(__html__, { width: 300, height: 300, themeColors: true });
 
 let collectionsData = [];
 
